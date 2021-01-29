ea9071c3d2a3e1dbe95324388fabb71f7075d142
atercia <15316878021@163.com>
2021-01-29 18:10:37 +0800

    learn - basic

---
+++ learn/fetch.ts
@@ -0,0 +1,6 @@
+const result = await fetch("https://deno.land/");
+/**
+ * @AIM set permit domain: github.com,deno.land
+ */
+// deno run --allow-net=github.com,deno.land ./learn/fetch.ts
+export { result }
